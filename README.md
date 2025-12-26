e9968599778cbe703793d278d6bcb4d687a967ab
artlu99 <alu99.sm@gmail.com>
2025-12-26 01:39:56 +0100

    add starlight site graph

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a simple Astro Starlight SSG built around Obsidian markdown files.
 
-h/t [starlight-obsidian](https://github.com/HiDeoo/starlight-obsidian) plugin by [HiDeoo](https://github.com/HiDeoo)!
+h/t [starlight-obsidian](https://github.com/HiDeoo/starlight-obsidian) plugin by [HiDeoo](https://github.com/HiDeoo) and [starlight-site-graph](https://github.com/fevol/starlight-site-graph) plugin by [Fevol](https://github.com/fevol)!
 
 ## RUNNING
 
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,32 @@ import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkBreaks from "remark-breaks";
 import starlightObsidian, { obsidianSidebarGroup } from "starlight-obsidian";
+import starlightSiteGraph from 'starlight-site-graph';
 import starlightThemeNova from "starlight-theme-nova";
 import { rehypePermalink } from "./src/plugins/rehype-permalink.ts";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://20251201-artlu.farchiver.xyz",
+	vite: {
+		define: {
+			// Polyfill for Node.js 'process' global used by micromatch/picomatch
+			'process.env': '{}',
+			'globalThis.process': JSON.stringify({
+				env: {},
+				versions: {},
+			}),
+		},
+		resolve: {
+			alias: {
+				// Polyfill for Node.js 'path' module used by micromatch/picomatch
+				path: 'path-browserify',
+			},
+		},
+		optimizeDeps: {
+			include: ['micromatch', 'path-browserify'],
+		},
+	},
 	markdown: {
 		remarkPlugins: [remarkBreaks],
 		rehypePlugins: [
@@ -28,6 +48,7 @@ export default defineConfig({
 			},
 			plugins: [
 				starlightThemeNova(/* options */),
+				starlightSiteGraph(),
 
 				// Generate the Obsidian vault pages.
 				starlightObsidian({
